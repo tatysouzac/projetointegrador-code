6a4c59c8c2122b1508b681b6bfe504bdb5ba378a
tatysouzac <47873452+tatysouzac@users.noreply.github.com>
2019-06-19 15:49:10 -0300

    arrumado layout editar

--- application/views/editar.php
+++ application/views/editar.php
@@ -1,25 +1,33 @@
-<center>
-<h1>
-</h1>
 
-	<div id="container">
+	<div id="corpo">
+		<br><br><br><br>
+			
+		<center>
+
+		<h1>Editar</h1>
+		<table  id="cadastroBox" border = "2">
+		<td align = "center">
 
 		<form action='http://localhost/integradorcode/index.php/welcome/enviar_editar' method='post'>
 
 			
 			<input type="hidden" name="id_cliente" value="<?= isset($id_cliente) ? $id_cliente : "" ?>">
-
+			NOME <br>
 			<input type="text" name="nome_cliente" placeholder="Nome" value="<?= isset($nome_cliente) ? $nome_cliente : "" ?>">
 			</br>
+			SOBRENOME <br>
 			<input type="text" name="sobrenome_cliente" placeholder="Some" value="<?= isset($sobrenome_cliente) ? $sobrenome_cliente : "" ?>" required>
-			</br>			
+			</br>
+			CPF <br>			
 			<input type="text" name="cpf_cliente" placeholder="CPF" value="<?= isset($cpf_cliente) ? $cpf_cliente : "" ?>" required>
 			</br>
+			EMAIL <br>
 			<input type="text" name="email_cliente" placeholder="Email" value="<?= isset($email_cliente) ? $email_cliente : "" ?>" required>
 			</br>
+			SENHA <br>
 			<input type="password" name="senha_cliente" placeholder="Senha" value="<?= isset($senha_cliente) ? $senha_cliente : "" ?>">
 
-			</br><input type="submit" name="btn_enviar" value="Editar">
+			</br><input type="submit" class="myButton" name="btn_enviar" value="Editar">
 		</form>
 	</div>
 </center>
